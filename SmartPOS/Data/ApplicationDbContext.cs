a9de0652ee2c63a2a937d60b5f01891c3b59e674
mdsrabonosd <mdsrabon.cse@gmail.com>
2026-06-25 02:06:40 +0600

    functionality

---
+++ SmartPOS/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SmartPOS.Models;
+
+namespace SmartPOS.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
